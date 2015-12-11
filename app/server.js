af92d5d4077b264d3d8434737c146c5bbbcc0d28
Artsem Yakimovich <artsemyakimovich@gmail.com>
2015-12-11 20:57:17 +0300

    Echo server was moved to separate module.

--- app/server.js
+++ app/server.js
@@ -1,37 +1,6 @@
-var http = require('http');
-var parse = require('url').parse;
-var join = require('path').join;
-var fs = require('fs');
+var echoServer = require("./server/echo_server");
 
 var root = __dirname;
 
-var server = http.createServer(function(req, res) {
-    var url = parse(req.url);
-    var path;
-    if (url.pathname == '/') {
-        path = join(root, 'index.html');
-    } else {
-        path = join(root, url.pathname);
-    }
-    fs.stat(path, function(err, stat) {
-        if (err) {
-            if ('ENOENT' == err.code) {
-                res.statusCode = 404;
-                res.end('Not Found');
-            } else {
-                res.statusCode = 500;
-                res.end('Internal Server Error');
-            }
-        } else {
-            res.setHeader('Content-Length', stat.size);
-            var stream = fs.createReadStream(path);
-            stream.pipe(res);
-            stream.on('error', function(err) {
-                res.statusCode = 500;
-                res.end('Internal Server Error');
-            });
-        }
-    });
-});
-
+var server = echoServer.createEchoServer(root);
 server.listen(3000);

54aab9645cc13c6bc3dfa81b71ea973fab1e9e72
Artsem Yakimovich <artemyakimovich@gmail.com>
2016-04-14 23:22:38 +0300

    ju0HjS33 Create model for Reservation entity.

--- api/reservations.js
+++ api/reservations.js
@@ -1,38 +1,44 @@
-var express = require('express');
-var router = express.Router();
+module.exports = function(sequelize) {
 
-router.route('/')
-    .all(function(req, res, next) {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        // res.writeHead(200, { 'Content-Type': 'application/json' });
-        next();
-    })
+    var express = require('express');
+    var Reservation = require('../models/reservation')(sequelize);
+    var router = express.Router();
 
-    .get(function(req, res, next) {
-        res.end('Get all reservations');
-    })
+    router.route('/')
+        .get(function (req, res, next) {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('Get all reservations');
+        })
 
-    .post(function(req, res, next) {
-        res.end('Add new reservation');
-    });
+        .post(function (req, res, next) {
+            var now = Date.now();
+            Reservation.create({
+                title: 'Test reservation',
+                description: 'test description',
+                startTime: now,
+                endTime: now
+            }).then(function(reservation) {
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end('Add new reservation with id: ' + reservation.id);
+            });
+        });
 
-router.route('/:id')
-    .all(function(req, res, next) {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        // res.writeHead(200, { 'Content-Type': 'application/json' });
-        next();
-    })
+    router.route('/:id')
+        .get(function (req, res, next) {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('Get reservation by ID = ' + req.params.id);
+        })
 
-    .get(function(req, res, next){
-        res.end('Get reservation by ID = ' + req.params.id);
-    })
+        .put(function (req, res, next) {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('Update reservation with ID = ' + req.params.id);
+        })
 
-    .put(function(req, res, next){
-        res.end('Update reservation with ID = ' + req.params.id);
-    })
+        .delete(function (req, res, next) {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
 
-    .delete(function(req, res, next){
-        res.end('Delete reservation with ID = ' + req.params.id);
-    });
+            res.end('Delete reservation with ID = ' + req.params.id);
+        });
 
-module.exports = router;
+    return router;
+}
